31488f98821b06fa168481479bafd3fca3bf1b47
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-12-11 19:25:32 +0500

    s

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Traits\LogsActivity;
 
 class Vendor extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = [
         'name',
